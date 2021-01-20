c0ee0bea3817576d471ade4e382c9bef71713e53
Afsharnive <aafsharn@PEPPERDINE.AD.PEPPERDINE.EDU>
2021-01-20 11:12:46 -0800

    updates contact us iframe

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,7 +4,7 @@ import './ContactForm.css';
 class ContactForm extends React.Component {
   render() {
     return (
-      <iframe title="Contact Form" seamless="seamless" frameborder="0" scrolling="no" width="100%" height="530px" src="http://clients.automanager.com/020310/custom-12/?framed=1"></iframe>
+      <iframe title="Contact Form" seamless="seamless" frameborder="0" scrolling="no" width="100%" height="530px" src="https://clients.automanager.com/020310/custom-12/?framed=1"></iframe>
     );
   }
 }

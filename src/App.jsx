28596c037695aaf3b504c0d5f40aed24413237ad
Andre Paiman Afsharnive <drepai@andres-mbp.mynetworksettings.com>
2024-03-19 04:07:24 -0400

    crear home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Navigation from './components/Nav/Nav';
-import Routes from './components/Routes/Routes';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component {
-  componentDidMount() {
-    document
-      .getElementById('container')
-      .setAttribute('style', `min-height: ${window.innerHeight}px`);
-
-    window.addEventListener('resize', () =>
-      document
-        .getElementById('container')
-        .setAttribute('style', `min-height: ${window.innerHeight}px`)
-    );
-  }
-
-  render() {
-    return (
-      <Container id="container">
-        <Navigation />
-        <div className="page-content">
-          <Routes />
-        </div>
-        <Footer />
-      </Container>
-    );
-  }
+function App() {
+  return (
+    <main
+      style={{
+        background: "#343a40",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100dvh",
+        flexDirection: "column",
+        gap: 28,
+      }}
+    >
+      <img
+        style={{ width: 400, height: 153 }}
+        src="/images/abreu-logo-v3.png"
+      />
+      <a href="https://www.911backdate.com" target="_blank" style={{}}>
+        <button
+          style={{
+            border: "2px solid white",
+            padding: "12px 32px",
+            borderRadius: "5px",
+            color: "white",
+            fontSize: 18,
+            letterSpacing: 3,
+          }}
+        >
+          COLLECTION
+        </button>
+      </a>
+    </main>
+  );
 }
 
 export default App;

1f112428f3a4123b1dfc155e76bb18c79240ae09
Andre Paiman Afsharnive <drepai@andres-mbp.mynetworksettings.com>
2024-03-19 11:47:03 -0400

    add home image

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,37 @@ function App() {
         minHeight: "100dvh",
         flexDirection: "column",
         gap: 28,
+        paddingBottom: 40,
       }}
     >
-      <img
-        style={{ width: 400, height: 153 }}
-        src="/images/abreu-logo-v3.png"
-      />
-      <a href="https://www.911backdate.com" target="_blank" style={{}}>
+      <img style={{ width: 400, height: 153 }} src="images/abreu-logo-v3.png" />
+      <div
+        style={{
+          maxHeight: 620,
+          display: "flex",
+          alignItems: "center",
+          overflow: "clip",
+          borderRadius: 12,
+        }}
+      >
+        <img
+          style={{ maxWidth: 1400, width: "100%" }}
+          src="images/car-profile.jpg"
+        />
+      </div>
+      <a href="https://www.911backdate.com" target="_blank">
         <button
           style={{
             border: "2px solid white",
-            padding: "12px 32px",
-            borderRadius: "5px",
+            padding: "12px 40px",
+            borderRadius: 6,
             color: "white",
-            fontSize: 18,
-            letterSpacing: 3,
+            fontSize: 24,
+            letterSpacing: 4,
+            cursor: "pointer",
           }}
         >
-          COLLECTION
+          911 BACKDATE
         </button>
       </a>
     </main>
